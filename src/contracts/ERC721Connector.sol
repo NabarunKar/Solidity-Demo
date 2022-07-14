0afdb889740eaffe7f42d7f10f12668dfe699b9f
NabarunKar <nabarunkar01@gmail.com>
2022-07-15 02:39:49 +0530

    updated kryptobirdz.sol and ERC721Connector

--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import './ERC721Metadata.sol';
+import './ERC721.sol'
 
-contract ERC721Connector is ERC721Metadata {
+contract ERC721Connector is ERC721Metadata, ERC721 {
     // constructor
     // the following constructor passes the name and symbol to the constructor of ERC721Metadata 
     constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol) {
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -5,6 +5,17 @@ import './ERC721Connector.sol';
 
 contract Kryptobird is ERC721Connector {
 
+    string[] public kryptoBirdz; // array to store our NFTs
+
+    function mint(string memory _kryptoBird) public {
+
+        kryptoBirdz.push(_kryptoBird);
+        uint _id = kryptoBirdz.length - 1;
+
+        //running the mint function from ERC721.sol:
+        _mint(msg.sender, _id);
+    }
+
     constructor() ERC721Connector('KryptoBird', 'KBIRDZ') {
         
 

b50b28272fc6f578a53e4a3cdeed421e3a99c6b1
NabarunKar <nabarunkar01@gmail.com>
2022-07-15 02:47:32 +0530

    Updated KryptoBirdz.sol with mapping and new requirements

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -7,13 +7,19 @@ contract Kryptobird is ERC721Connector {
 
     string[] public kryptoBirdz; // array to store our NFTs
 
+    mapping(string => bool) _kryptoBirdzExists;
+
     function mint(string memory _kryptoBird) public {
+
+        //require that the current kryptoBird being minted does not already exist
+        require(!_kryptoBirdzExists[_kryptoBird], 'Error - kryptoBird already exists!');
 
         kryptoBirdz.push(_kryptoBird);
         uint _id = kryptoBirdz.length - 1;
 
         //running the mint function from ERC721.sol:
         _mint(msg.sender, _id);
+        _kryptoBirdzExists[_kryptoBird] = true;
     }
 
     constructor() ERC721Connector('KryptoBird', 'KBIRDZ') {

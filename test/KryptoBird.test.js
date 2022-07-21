e255ac308fd8aa0e214e63bc3763191d06cbf45a
NabarunKar <nabarunkar01@gmail.com>
2022-07-21 18:38:21 +0530

    3 tests done - deploys successfully, name and symbol

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -15,6 +15,8 @@ contract('KryptoBird', (accounts) => { // grabs the ganache accounts
     describe('deployment', async() => {
         // test samples with writing it
         // the following tests whether or not it has deployed successfully
+
+        //1.
         it('deploys successfully', async() => {
             contract  = await KryptoBird.deployed()
             const address = contract.address; // grabs the contract address
@@ -23,6 +25,16 @@ contract('KryptoBird', (accounts) => { // grabs the ganache accounts
             assert.notEqual(address, null)     // or null 
             assert.notEqual(address, undefined)// or undefined
             assert.notEqual(address, 0x0)      // or 0x0
+        })
+        //2.
+        it('has a name', async() => {
+            const name = await contract.name()
+            assert.equal(name, 'KryptoBird') // we're checking if the name is still equal to what we set it to in KryptoBirdz.sol
+        })
+        // 3.
+        it('has a symbol', async() => {
+            const symbol = await contract.symbol()
+            assert.equal(symbol, 'KBIRDZ') // we're checking if the symbol is still equal to what we set it to in KryptoBirdz.sol
         })
     })
 })
